72de2c107947a5ec91f082ee3277b9cbf6b8c304
Chris Portela <cp@0xa.io>
2023-09-15 11:53:48 -0400

    Updated ssh keys and flake lock

--- lib/sshKeys.nix
+++ lib/sshKeys.nix
@@ -1,5 +1,5 @@
 let
-  mbaHomeSE = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLKmP5UUboT3SkiyHzY81/7UGG0SrVcSWxywkD8lpxYznrFz2uWT6zGfiQNj8FrLSwrh/AthIZJfe0LvbKEtTq8= home@secretive.cp-mba.local";
+  mbaHomeSE = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHN+RyedBKQ/UStTC2anytbwhHI10PPDfpfJLpTtTvlPRw4mhw95TaQlUfOmT3kTB3YmNzbCbssFW5zR9ZbKY3s= home@secretive.lux.local";
   nixKeyWindows = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII5kFjpHHMhPxXAp54egnvuGVidd0g83jrw9AzD3AB5N cp@cp-win1";
 in
 {
