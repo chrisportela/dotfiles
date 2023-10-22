1f06687fbb93c990d0cca2efeb93dc4f077a4991
Chris Portela <chris@chrisportela.com>
2023-10-22 15:16:59 -0400

    Update commented out option for pipewire

--- hosts/ada.nix
+++ hosts/ada.nix
@@ -254,7 +254,7 @@ inputs.nixpkgs.lib.nixosSystem {
 
         # use the example session manager (no others are packaged yet so this is enabled by default, no
         # need to redefine it in your config for now)
-        #media-session.enable = true;
+        #wireplumber.enable = true;
       };
 
       programs.neovim = {
