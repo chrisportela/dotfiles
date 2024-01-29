47bf79528f2e3063ce601416f037542f711f90d2
Chris Portela <cp@0xa.io>
2024-01-29 10:36:48 -0500

    Disabled macos sandbox

--- lib/darwin/nixpkgs.nix
+++ lib/darwin/nixpkgs.nix
@@ -1,6 +1,6 @@
 { lib, config, pkgs, inputs, nixpkgs, overlays, ... }: with lib; {
   nix = {
-    package = mkDefault pkgs.nixVersions.nix_2_18;
+    package = mkDefault pkgs.nixVersions.nix_2_19;
     registry.nixpkgs.flake = nixpkgs;
     configureBuildUsers = true;
 
@@ -8,8 +8,8 @@
       keep-outputs = mkDefault true;
       keep-derivations = mkDefault true;
       experimental-features = [ "nix-command" "flakes" ];
-      sandbox = "relaxed";
-      trusted-users = mkDefault [ "root" "cmp" ];
+      sandbox = false;
+      trusted-users = mkDefault [ "root" "@admin" ];
       extra-trusted-public-keys = mkDefault [
         "binarycache.cp-mba.local:xH/m5WHjOty8a0/n27WSKGhNC0eDf/HX6GREG+G6czM="
         "cache.cp-mba.local-1:YJIH05Ett5Tcq2eEyfroindEQdpwBG5F5f7ztZ+gFCw="
