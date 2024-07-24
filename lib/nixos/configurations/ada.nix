0dab2332ff72caacb80412be6bd5a929113cd8fb
Chris Portela <chris@chrisportela.com>
2024-07-23 21:53:11 -0400

    Refactored ada config to use new modules

--- lib/nixos/configurations/ada.nix
+++ lib/nixos/configurations/ada.nix
@@ -1,82 +1,45 @@
-{ inputs, nixosModules, overlays ? [ ], ... }:
+{ inputs, overlays ? [ ], ... }:
 let
-  hardwareConfig = import ../hardware/ada.nix;
+  nginx = { ... }: { };
 in
 inputs.nixos.lib.nixosSystem {
   specialArgs = {
-    inherit inputs overlays;
+    inherit inputs;
     nixpkgs = inputs.nixos;
   };
 
   modules = [
     inputs.vscode-server.nixosModules.default
-    nixosModules.common
-    # nixosModules.ddc  # is, In-Fact, The Source of udev boot slowness
-    nixosModules.dualboot
-    nixosModules.nixpkgs
-    nixosModules.openssh
-    hardwareConfig
+    ../hardware/ada.nix
+
+    ../modules/nixpkgs.nix
+    ../modules/common.nix
+    ../modules/network.nix
+    ../modules/openssh.nix
+    ../modules/cafecitocloud
+    ../modules/gaming.nix
+    ../modules/local-llm
 
     ({ pkgs, config, lib, ... }: {
 
       nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-        "steam"
-        "steam-original"
-        "steam-run"
-        "nvidia-persistenced"
-        "nvidia-settings"
-        "nvidia-x11"
-        "cuda_cudart"
-        "cuda-merged"
-        "cuda_cuobjdump"
-        "cuda_gdb"
-        "cuda_nvcc"
-        "cuda_nvdisasm"
-        "cuda_nvprune"
-        "cuda_cccl"
-        "cuda_cupti"
-        "cuda_cuxxfilt"
-        "cuda_nvml_dev"
-        "cuda_nvrtc"
-        "cuda_nvtx"
-        "cuda_profiler_api"
-        "cuda_sanitizer_api"
-        "libcublas"
-        "libcufft"
-        "libcurand"
-        "libcusolver"
-        "libnvjitlink"
-        "libcusparse"
-        "libnpp"
         "1password"
         "1password-cli"
         "ookla-speedtest"
       ];
 
-      security.acme = {
-        acceptTerms = true;
-        defaults = {
-          dnsResolver = "liara.gorgon-basilisk.ts.net";
-          email = "chris@cafecito.cloud";
-          server = "https://ca.cafecito.cloud/acme/acme/directory";
-          webroot = "/var/lib/acme/acme-challenge";
+      cafecitocloud.enable = true;
+      chrisportela = {
+        common.enable = true;
+        network = {
+          enable = true;
+          speedtest-utils = true;
         };
+        gaming.enble = false;
+        local-llm.enable = true;
       };
 
-      services.pcscd.enable = true;
-
-      security.pki.certificates = [
-        ''
-          Root Cafecito Cloud CA
-          =======================
-          ${builtins.readFile ../../cafecitocloud-root_ca.crt}
-        ''
-        ''
-          YubiKey 4 Cafecito Cloud Intermediate CA
-          =======================
-          ${builtins.readFile ../../cafecitocloud-yubikey4-intermediate_ca.crt}
-        ''
-      ];
+      services.pcscd.enable = true; # For configuring Yubikey
 
       # Enable networking
       networking = {
@@ -88,8 +51,6 @@ inputs.nixos.lib.nixosSystem {
         useDHCP = false;
         useNetworkd = true;
         networkmanager.enable = true;
-
-        # nameservers = [ "1.1.1.1#853" ];
 
         interfaces.enp6s0.useDHCP = true;
         interfaces.wlo1.useDHCP = true;
@@ -111,11 +72,8 @@ inputs.nixos.lib.nixosSystem {
       environment.systemPackages = with pkgs; [
         btop
         nvtopPackages.full
-        virt-manager
 
         # Hardware
-        ddcutil
-        ddcui
         lm_sensors
         pciutils
         glxinfo
@@ -123,77 +81,16 @@ inputs.nixos.lib.nixosSystem {
 
         # Network
         inetutils
-        ipcalc
-        iperf3
         nftables
         tcpdump
         traceroute
-        fast-cli
-        ookla-speedtest
-        speedtest-cli
       ];
 
-      programs.git = {
-        enable = true;
-        lfs.enable = true;
-      };
-
-      services.tailscale = {
-        enable = true;
-        package = pkgs.tailscale;
-      };
-
-      services.ollama = {
-        enable = true;
-        acceleration = "cuda";
-        listenAddress = "127.0.0.1:11434";
-        environmentVariables = {
-          OLLAMA_ORIGINS = "https://ollama.ada.i.cafecito.cloud";
-          OLLAMA_KEEP_ALIVE = "12h";
-        };
-      };
-
-      systemd.services.ollama-reload =
-        let
-          script = (pkgs.writeShellScriptBin "reload-ollama" ''
-            systemctl stop ollama
-            echo "Ollama service stopped"
-
-            echo "Reloading Nvidia kernel modules"
-            ${pkgs.kmod}/bin/rmmod nvidia_uvm && ${pkgs.kmod}/bin/modprobe nvidia_uvm
-
-            echo "Starting Ollama service..."
-            systemctl start ollama
-          '');
-        in
-        {
-          enable = true;
-          description = "Reloads NVidia kernel modules and restarts ollama so it can use GPU after suspend.";
-          after = [ "suspend.target" ];
-          wantedBy = [ "suspend.target" ];
-          serviceConfig = {
-            Type = "simple";
-            ExecStart = "${script}/bin/reload-ollama";
-          };
-        };
-
-      services.resolved = {
-        enable = true;
-        fallbackDns = [
-          "1.1.1.1#853"
-        ];
-        dnssec = "false";
-      };
-
-      time.timeZone = "America/New_York";
+      time.timeZone = lib.mkForce "America/New_York";
 
       specialisation.desktop.configuration = {
+        chrisportela.gaming.enable = true;
 
-        environment.systemPackages = with pkgs; [
-          (lutris.override {
-            extraLibraries = pkgs: [ ];
-          })
-        ];
         services.xserver = {
           # Enable the X11 windowing system.
           # enable = true;
@@ -226,8 +123,7 @@ inputs.nixos.lib.nixosSystem {
         };
         programs.xwayland.enable = true;
 
-        # Enable CUPS to print documents.
-        services.printing.enable = true;
+        services.printing.enable = false;
 
         # Enable sound with pipewire.
         sound.enable = true;
@@ -238,39 +134,37 @@ inputs.nixos.lib.nixosSystem {
           alsa.enable = true;
           alsa.support32Bit = true;
           pulse.enable = true;
-          # If you want to use JACK applications, uncomment this
-          # jack.enable = true;
-
-          # use the example session manager (no others are packaged yet so this is enabled by default, no
-          # need to redefine it in your config for now)
-          # wireplumber.enable = true;
         };
 
         programs._1password.enable = true;
-        programs._1password-gui.enable = true;
-        programs._1password-gui.polkitPolicyOwners = [ "cmp" ];
+        programs._1password-gui = {
+          enable = true;
+          polkitPolicyOwners = [ "cmp" ];
+        };
         security.pam.services.kwallet.enableKwallet = true;
 
-
-        programs.steam = {
-          enable = true;
-          remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-          dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
-          gamescopeSession.enable = true;
-        };
-
         virtualisation = {
-          virtualbox.host = {
-            enable = true;
-          };
+          virtualbox.host.enable = true;
           libvirtd.enable = true;
         };
         programs.dconf.enable = true; # For libvirtd
 
         environment.sessionVariables = {
-          STEAM_FORCE_DESKTOPUI_SCALING = "2";
           NIXOS_OZONE_WL = "1";
         };
+
+        users.users.cmp = {
+          extraGroups = [
+            "libvirtd"
+          ];
+
+          packages = with pkgs; [
+            firefox
+            kate
+            virt-manager
+          ];
+        };
+
       };
 
       boot.plymouth = {
@@ -279,17 +173,12 @@ inputs.nixos.lib.nixosSystem {
         # theme = "bgrt";
       };
 
-      programs.neovim = {
-        enable = true;
-        viAlias = true;
-        vimAlias = true;
-      };
-
       services.vscode-server.enable = lib.mkDefault true;
 
       services.zfs = {
         trim = {
           enable = true;
+          # interval = "daily";
         };
 
         autoScrub = {
@@ -306,68 +195,12 @@ inputs.nixos.lib.nixosSystem {
           enableNvidia = true;
           storageDriver = "zfs";
         };
+        # TODO: switch to podman
         oci-containers.backend = "docker";
-        # virtualbox.host = {
-        #   enable = true;
-        # };
-        # libvirtd.enable = true;
-      };
-      # programs.dconf.enable = true; # For libvirtd
-
-      # Ollama Web UI
-      virtualisation.oci-containers.containers.open-webui = {
-        autoStart = true;
-        image = "ghcr.io/open-webui/open-webui";
-        # TODO figure out how to create the data directory declaratively
-        volumes = [ "${config.users.users.cmp.home}/open-webui:/app/backend/data" ];
-        extraOptions = [ "--network=host" "--add-host=host.containers.internal:host-gateway" ];
-        environment = {
-          OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-          OLLAMA_BASE_URL = "http://127.0.0.1:11434";
-        };
       };
 
-
+      services.nginx.enable = true;
       users.users.nginx.extraGroups = [ "acme" ];
-
-      services.nginx = {
-        enable = true;
-
-        virtualHosts = {
-          "ollama.ada.i.cafecito.cloud" = {
-            forceSSL = true;
-            enableACME = true;
-
-            locations."/" = {
-              proxyPass = "http://127.0.0.1:11434";
-              #recommendedProxySettings = true;
-              extraConfig = ''
-                proxy_set_header Host localhost:11434;
-              '';
-            };
-
-            extraConfig = ''
-              access_log /var/log/nginx/ollama-cafeito_cloud.access.log;
-              error_log /var/log/nginx/ollama-cafeito_cloud.error.log;
-            '';
-          };
-
-          "chat.ada.i.cafecito.cloud" = {
-            forceSSL = true;
-            enableACME = true;
-
-            locations."/" = {
-              proxyPass = "http://127.0.0.1:8080";
-              recommendedProxySettings = true;
-            };
-
-            extraConfig = ''
-              access_log /var/log/nginx/chat-cafeito_cloud.access.log;
-              error_log /var/log/nginx/chat-cafeito_cloud.error.log;
-            '';
-          };
-        };
-      };
 
       users.users.cmp = {
         extraGroups = [
@@ -378,7 +211,7 @@ inputs.nixos.lib.nixosSystem {
           "docker"
         ];
 
-        packages = with pkgs; [ firefox kate ];
+        packages = with pkgs; [ ];
       };
 
       system.stateVersion = "23.05";
