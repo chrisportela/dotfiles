526a7fe7c5df00aa1d150d9e159e99edd9dd13df
Chris Portela <chris@chrisportela.com>
2024-09-04 17:19:58 -0400

    Removed offline install
    Flamme build is too big for an ISO based install.

--- lib/nixos/configurations/installer.nix
+++ lib/nixos/configurations/installer.nix
@@ -1,15 +1,6 @@
 { inputs, system ? "x86_64-linux", hostName ? "installer", ... }:
 let
   sshKeys = import ../../sshKeys.nix;
-  dependencies = [
-    self.nixosConfigurations.flamme.config.system.build.toplevel
-    self.nixosConfigurations.flamme.config.system.build.diskoScript
-    self.nixosConfigurations.flamme.config.system.build.diskoScript.drvPath
-    self.nixosConfigurations.flamme.pkgs.stdenv.drvPath
-    (self.nixosConfigurations.flamme.pkgs.closureInfo { rootPaths = [ ]; }).drvPath
-  ] ++ builtins.map (i: i.outPath) (builtins.attrValues self.inputs);
-
-  closureInfo = pkgs.closureInfo { rootPaths = dependencies; };
 in
 inputs.nixos.lib.nixosSystem {
   inherit system;
@@ -21,37 +12,30 @@ inputs.nixos.lib.nixosSystem {
     ../modules/nixpkgs.nix
     ({ pkgs, lib, ... }: {
 
-      # nixpkgs.hostPlatform.system = system;
-      networking.hostName = hostName;
+        # nixpkgs.hostPlatform.system = system;
+        networking.hostName = hostName;
 
-      boot.loader.timeout = lib.mkOverride 10 10;
-      documentation.enable = lib.mkOverride 10 false;
-      documentation.nixos.enable = lib.mkOverride 10 false;
+        boot.loader.timeout = lib.mkOverride 10 10;
+        documentation.enable = lib.mkOverride 10 false;
+        documentation.nixos.enable = lib.mkOverride 10 false;
 
-      boot.initrd.systemd.enable = lib.mkForce false;
+        boot.initrd.systemd.enable = lib.mkForce false;
 
-      system.disableInstallerTools = lib.mkOverride 10 false;
+        system.disableInstallerTools = lib.mkOverride 10 false;
 
-      systemd.services.sshd.wantedBy = pkgs.lib.mkOverride 10 [ "multi-user.target" ];
+        systemd.services.sshd.wantedBy = pkgs.lib.mkOverride 10 [ "multi-user.target" ];
 
-      environment.systemPackages = [
-        inputs.disko.packages.${system}.disko
-        inputs.disko.packages.${system}.disko-install
-        (pkgs.writeShellScriptBin "install-nixos-unattended" ''
-          set -eux
-          # Replace "/dev/disk/by-id/some-disk-id" with your actual disk ID
-          exec ${pkgs.disko}/bin/disko-install --flake "${self}#flamme"
-        '')
-      ];
+        environment.systemPackages = [
+          inputs.disko.packages.${system}.disko
+          inputs.disko.packages.${system}.disko-install
+        ];
 
-      environment.etc."install-closure".source = "${closureInfo}/store-paths";
-
-      users.groups.nix = { };
-      users.users.nix = {
-        isSystemUser = true;
-        group = "nix";
-        openssh.authorizedKeys.keys = sshKeys.default;
-      };
-    })
+        users.groups.nix = { };
+        users.users.nix = {
+          isSystemUser = true;
+          group = "nix";
+          openssh.authorizedKeys.keys = sshKeys.default;
+        };
+      })
   ];
 }
