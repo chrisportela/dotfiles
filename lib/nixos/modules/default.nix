2099a375713b53769f009f0c02ff3f244c361dc8
Chris Portela <chris@chrisportela.com>
2024-09-24 13:42:55 -0400

    Enabled more modules to be exported

--- lib/nixos/modules/default.nix
+++ lib/nixos/modules/default.nix
@@ -1,7 +1,11 @@
 {
+  cafecitocloud = ./cafecitocloud;
   common = ./common.nix;
   # ddc = ./ddc.nix;
+  local-llm = ./local-llm;
+  gaming = ./gaming.nix;
   network = ./network.nix;
+  nixpkgs = ./nixpkgs.nix;
   nginx-cloudflare = ./nginx-cloudflare.nix;
   openssh = ./openssh.nix;
 }
