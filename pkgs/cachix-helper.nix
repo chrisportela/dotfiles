228d1dad3dbade5ac56817c83e43cea389d0a66f
Chris Portela <cp@0xa.io>
2025-01-11 14:57:47 -0500

    Fixed cachix helper script

--- pkgs/cachix-helper.nix
+++ pkgs/cachix-helper.nix
@@ -12,20 +12,21 @@ in (pkgs.writeShellScriptBin "cachix-helper" ''
   else
     nix build .#legacyPackages.${stdenv.system}.homeConfigurations.${hmConfig}.activationPackage
   fi
+  rm result-hm-cmp || true
   mv result result-hm-cmp
   cachix push ${cachixArgs} ${cachixRepo} result-hm-cmp
   cachix pin ${cachixRepo} home-manager-${stdenv.system} result-hm-cmp
 
   echo "#### Building shells"
-  nix build --out result-shell-dotfiles .#devShells.${stdenv.system}.dotfiles
+  nix build --out-link result-shell-dotfiles .#devShells.${stdenv.system}.dotfiles
   cachix push ${cachixArgs} ${cachixRepo} result-shell-dotfiles
   cachix pin ${cachixRepo} shell-dotfiles-${stdenv.system} result-shell-dotfiles
 
-  nix build --out result-shell-devops .#devShells.${stdenv.system}.devops
+  nix build --out-link result-shell-devops .#devShells.${stdenv.system}.devops
   cachix push ${cachixArgs} ${cachixRepo} result-shell-devops
   cachix pin ${cachixRepo} shell-devops-${stdenv.system} result-shell-devops
 
-  nix build --out result-shell-dev .#devShells.${stdenv.system}.dev
+  nix build --out-link result-shell-dev .#devShells.${stdenv.system}.dev
   cachix push ${cachixArgs} ${cachixRepo} result-shell-dev
   cachix pin ${cachixRepo} shell-dev-${stdenv.system} result-shell-dev
 
