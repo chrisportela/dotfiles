c136cfa2b892fb69e9b0e6144fa86e75b74ab9d3
Chris Portela <chris@chrisportela.com>
2025-01-30 18:10:47 -0500

    Tune ollama config on ada

--- lib/nixos/modules/local-llm/default.nix
+++ lib/nixos/modules/local-llm/default.nix
@@ -46,9 +46,9 @@ with lib;
       port = 11434;
       environmentVariables = {
         OLLAMA_ORIGINS = "https://ollama.ada.i.cafecito.cloud";
-        OLLAMA_KEEP_ALIVE = "12h";
-        OLLAMA_NUM_PARALLEL = "4";
-        OLLAMA_MAX_LOADED_MODELS = "5";
+        OLLAMA_KEEP_ALIVE = "48h";
+        OLLAMA_NUM_PARALLEL = "2";
+        OLLAMA_MAX_LOADED_MODELS = "3";
         OLLAMA_MAX_QUEUE = "1";
       };
     };
