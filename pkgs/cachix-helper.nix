0b8a6505b64115c255e2656c8ee4a469c9edd56c
Chris Portela <chris@chrisportela.com>
2025-06-10 15:55:44 -0400

    Improved cachix helper script with specific drv paths

--- pkgs/cachix-helper.nix
+++ pkgs/cachix-helper.nix
@@ -12,9 +12,10 @@ let
     "--compression-method zstd"
     "--jobs 4"
   ];
+  nixBin = "${nix}/bin/nix";
+  cachixBin = "${cachix}/bin/cachix";
   hmConfig = "cmp";
 in
-# shells = ["dotfiles", "dev", "devops"];
 (pkgs.writeShellScriptBin "cachix-helper" ''
   set -eu
   SYSTEM="${stdenv.system}"
@@ -22,28 +23,28 @@ in
     SYSTEM="$1"
   fi
   echo "#### Building HM"
-  if command -v tailscale 1>/dev/null 2>&1; then
+  if command -v home-manager 1>/dev/null 2>&1; then
     home-manager build --flake .#${hmConfig}
   else
-    nix build .#legacyPackages.$SYSTEM.homeConfigurations.${hmConfig}.activationPackage
+    ${nixBin} build .#legacyPackages.$SYSTEM.homeConfigurations.${hmConfig}.activationPackage
   fi
   rm result-hm-cmp || true
   mv result result-hm-cmp
-  cachix push ${cachixArgs} ${cachixRepo} result-hm-cmp
-  cachix pin ${cachixRepo} home-manager-$SYSTEM result-hm-cmp
+  ${cachixBin} push ${cachixArgs} ${cachixRepo} result-hm-cmp
+  ${cachixBin} pin ${cachixRepo} home-manager-$SYSTEM result-hm-cmp
 
   echo "#### Building shells"
-  nix build --out-link result-shell-dotfiles .#devShells.$SYSTEM.dotfiles
-  cachix push ${cachixArgs} ${cachixRepo} result-shell-dotfiles
-  cachix pin ${cachixRepo} shell-dotfiles-$SYSTEM result-shell-dotfiles
+  ${nixBin} build --out-link result-shell-dotfiles .#devShells.$SYSTEM.dotfiles
+  ${cachixBin} push ${cachixArgs} ${cachixRepo} result-shell-dotfiles
+  ${cachixBin} pin ${cachixRepo} shell-dotfiles-$SYSTEM result-shell-dotfiles
 
-  nix build --out-link result-shell-devops .#devShells.$SYSTEM.devops
-  cachix push ${cachixArgs} ${cachixRepo} result-shell-devops
-  cachix pin ${cachixRepo} shell-devops-$SYSTEM result-shell-devops
+  ${nixBin} build --out-link result-shell-devops .#devShells.$SYSTEM.devops
+  ${cachixBin} push ${cachixArgs} ${cachixRepo} result-shell-devops
+  ${cachixBin} pin ${cachixRepo} shell-devops-$SYSTEM result-shell-devops
 
-  nix build --out-link result-shell-dev .#devShells.$SYSTEM.dev
-  cachix push ${cachixArgs} ${cachixRepo} result-shell-dev
-  cachix pin ${cachixRepo} shell-dev-$SYSTEM result-shell-dev
+  ${nixBin} build --out-link result-shell-dev .#devShells.$SYSTEM.dev
+  ${cachixBin} push ${cachixArgs} ${cachixRepo} result-shell-dev
+  ${cachixBin} pin ${cachixRepo} shell-dev-$SYSTEM result-shell-dev
 
   echo "#### Finished!"
 '')
