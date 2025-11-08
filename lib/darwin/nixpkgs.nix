3baa68a553d54ce70464dfcc1a8e098dee148db9
Chris Portela <chris@chrisportela.com>
2025-11-08 13:46:04 -0500

    Use older nix version until deploy-rs fixed

--- lib/darwin/nixpkgs.nix
+++ lib/darwin/nixpkgs.nix
@@ -1,10 +1,11 @@
-{ lib, config, ... }:
+{ lib, pkgs, config, ... }:
 {
   imports = [ ../nixos/modules/nixpkgs.nix ];
 
   config = {
 
     nix = {
+      package = pkgs.nixVersions.nix_2_31;
       settings = {
         sandbox = false; # Even relaxed prevents HM builds - https://github.com/NixOS/nix/issues/4119 (2020)
         trusted-users = [
