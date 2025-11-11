4ddd6e449e5ef7c4611aaa185592a5f9717bef07
Chris Portela <chris@chrisportela.com>
2025-11-10 19:01:18 -0500

    Added allowedUnfree option to darwin config

--- lib/darwin/nixpkgs.nix
+++ lib/darwin/nixpkgs.nix
@@ -2,7 +2,15 @@
 {
   imports = [ ../nixos/modules/nixpkgs.nix ];
 
+  options.allowedUnfree = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    default = [ ];
+  };
+
   config = {
+    nixpkgs.config.allowUnfreePredicate = lib.mkForce (
+      p: builtins.elem (lib.getName p) config.allowedUnfree
+    );
 
     nix = {
       package = pkgs.nixVersions.nix_2_31;
