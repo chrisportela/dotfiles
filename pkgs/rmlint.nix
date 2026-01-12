effd632d70108f65baef442e7c5de9b9e2777eb3
Chris Portela <chris@chrisportela.com>
2026-01-12 16:51:30 -0500

    fixed rmlint build with zfs reflink support

--- pkgs/rmlint.nix
+++ pkgs/rmlint.nix
@@ -1,8 +1,9 @@
 { rmlint, fetchpatch }:
 let
   zfs-support-patch = fetchpatch {
-    url = "https://patch-diff.githubusercontent.com/raw/sahib/rmlint/pull/748.patch";
-    sha256 = "sha256-ODHcc3U9/b8WDYPRSSOXV5Ggjr1ZQbyX9NVGVumA8xs=";
+    #url = "https://patch-diff.githubusercontent.com/raw/sahib/rmlint/pull/748.patch";
+    url = "https://github.com/sahib/rmlint/compare/master...99b61cadb1105ae9b9e9360326f581e0c9a4a4c4.patch?full_index=1";
+    sha256 = "sha256-NngZQvLcQSd6FYDlyFnhAfJ8k29SKUa+Uf7IsDfQItI=";
   };
 in
 rmlint.overrideAttrs (
