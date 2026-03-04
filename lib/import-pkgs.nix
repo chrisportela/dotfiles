fe53aa8a4ea7f3227439a4e9ff46a891f1a9ede3
Chris Portela <chris@chrisportela.com>
2026-03-04 12:33:14 -0500

    import-pkgs accepts allowedUnfree and extra overlays

--- lib/import-pkgs.nix
+++ lib/import-pkgs.nix
@@ -1,49 +1,28 @@
 # Build pkgs/pkgsUnstable for a given system with overlays and unfree. Called as:
 # importPkgs = (import ./lib/import-pkgs.nix) { inherit self nixpkgs nixpkgs-unstable inputs; };
-{ self, nixpkgs, nixpkgs-unstable, inputs }:
+{ self, nixpkgs, nixpkgs-unstable, inputs, allowUnfree ? [], overlays ? [ ] }:
 system:
 let
-  overlays = with (import ../overlays/default.nix { inherit self inputs; }); [
+  overlays' = overlays ++ (with (import ../overlays/default.nix { inherit self inputs; }); [
     rust
     rustToolchain
     deploy-rs
     terraform
     setup-envrc
     opencode-cursor
-  ];
-  unfreePredicate = (
-    pkg:
-    builtins.elem (nixpkgs.lib.getName pkg) [
-      "terraform"
-      "vault-bin"
-      "claude-code"
-    ]
-  );
-  nixosUnstablePkgs = import inputs.nixpkgs-unstable {
-    inherit system;
-    config.allowUnfreePredicate = unfreePredicate;
-  };
-in
-rec {
+  ]);
+  unfreePredicate = (pkg: builtins.elem (nixpkgs.lib.getName pkg) allowUnfree);
+in rec {
   pkgsUnstable = import nixpkgs-unstable {
     inherit system;
     config.allowUnfreePredicate = unfreePredicate;
-    overlays = overlays ++ [
-      (final: prev: {
-        claude-code = self.packages.${system}.claude-code;
-      })
-    ];
+    overlays = overlays';
   };
   pkgs = import nixpkgs {
     inherit system;
     config.allowUnfreePredicate = unfreePredicate;
-    overlays = overlays ++ [
-      (final: prev: {
-        pkgsUnstable = pkgsUnstable;
-      })
-      (final: prev: {
-        claude-code = self.packages.${system}.claude-code;
-      })
-    ];
+    overlays = [
+      (final: prev: { pkgsUnstable = pkgsUnstable; })
+    ] ++ overlays';
   };
 }
