94c3016423fbb0d1ab5e15ece444226490e60986
Chris Portela <chris@chrisportela.com>
2026-03-19 11:30:34 -0400

    Add sambda password

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,8 @@
 let
   sshKeys = import ../lib/ssh-keys.nix;
+  ada = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILsqpaOSjCbxoTry3oYRHElBMbnFvZVVa5sxjbTZO/lX cmp@ada";
 in
 {
   "example.age".publicKeys = sshKeys.secrets ++ [ ];
+  "ada-samba-passwords.age".publicKeys = sshKeys.secrets ++ [ ada ];
 }
