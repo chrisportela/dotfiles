5c83ec3b52c902b9320f552de2ade3a6d6f847ab
Chris Portela <chris@chrisportela.com>
2026-03-20 02:43:58 -0400

    Fixed encryption for deploying to ada

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
   sshKeys = import ../lib/ssh-keys.nix;
   ada = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILsqpaOSjCbxoTry3oYRHElBMbnFvZVVa5sxjbTZO/lX cmp@ada";
+  adaHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINzAkODt0f1rcTToP7ajlylJtj4IfmyLgCW8yE5ze+UT root@ada";
 in
 {
   "example.age".publicKeys = sshKeys.secrets ++ [ ];
-  "ada-samba-passwords.age".publicKeys = sshKeys.secrets ++ [ ada ];
+  "ada-samba-passwords.age".publicKeys = sshKeys.secrets ++ [ ada adaHost ];
 }
